62ceb43ab5ed473997b66eb6abf35cc95e97d6c2
郑峰涛 <Color@Color-MBP.local>
2016-05-30 22:27:45 +0800

    change background color

--- EditorGUIExercise/Assets/Script/Editor/ToolBarItem.cs
+++ EditorGUIExercise/Assets/Script/Editor/ToolBarItem.cs
@@ -10,8 +10,23 @@ public class ToolBarItemWindow : EditorWindow {
 	}
 
 	void OnGUI() {
+
+		Color backupColor = GUI.backgroundColor;
+
 		GUILayout.BeginHorizontal(EditorStyles.toolbar, GUILayout.ExpandWidth(true));
-		GUILayout.Button("Hello", EditorStyles.toolbarButton, GUILayout.ExpandWidth(false));
+		{
+			GUI.backgroundColor = Color.green;
+			for(int i = 0; i < 10; ++i) {
+				if (GUILayout.Button("Button" + i, EditorStyles.toolbarButton, GUILayout.ExpandWidth(false))) {
+					EditorUtility.DisplayDialog("hello", "Button click is " + i, "确定"); 
+				}
+			}
+		}
+		GUI.backgroundColor = backupColor;
+
+		GUILayout.Button("Test Color", EditorStyles.toolbarButton, GUILayout.ExpandWidth(false));
+
+			
 		GUILayout.EndHorizontal();
 			
 	}
