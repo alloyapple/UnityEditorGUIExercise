f8fb037f3b397f3e9e96510bcfb14634eb04b1f1
郑峰涛 <Color@Color-MBP.local>
2016-06-21 07:29:27 +0800

    add serializable

--- EditorGUIExercise/Assets/Script/WeaponItem.cs
+++ EditorGUIExercise/Assets/Script/WeaponItem.cs
@@ -1,9 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[System.Serializable]
+public class FooBar {
+
+	public int foo = 0;
+	[System.NonSerialized]
+	public int bar = 2;
+	
+}
+
 public class WeaponItem : MonoBehaviour {
 
 	// Use this for initialization
+
+	public FooBar fooBar;
+
 	void Start () {
 	
 	}
