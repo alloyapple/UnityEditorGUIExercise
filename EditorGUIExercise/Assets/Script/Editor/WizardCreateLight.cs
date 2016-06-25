f8a4a427b591859570d5b43ea5b3a64725f9378e
郑峰涛 <Color@Color-MBP.local>
2016-06-25 08:59:46 +0800

    WizardCreateLight

---
+++ EditorGUIExercise/Assets/Script/Editor/WizardCreateLight.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+using System.Collections;
+
+public class WizardCreateLight : ScriptableWizard {
+
+	public float range = 500;
+	public Color color = Color.red;
+
+	[MenuItem("GameObject/Create Light Wizard")]
+	static void CreateWizard() {
+		ScriptableWizard.DisplayWizard<WizardCreateLight>("Create light", "Create", "Apply");
+
+	}
+
+	void OnWizardCreate() {
+		GameObject go = new GameObject("New Light");
+		Light lt = go.AddComponent<Light>();
+		lt.range = range;
+		lt.color = color;
+	}
+
+	void OnWizardUpdate() {
+		helpString = "Please set light color";
+	}
+
+	void OnWizardOtherButton() {
+		if (Selection.activeTransform != null) {
+			Light lt = Selection.activeTransform.GetComponent<Light>();
+			if (lt != null) {
+				lt.color = Color.red;
+			}
+		}
+	}
+}
